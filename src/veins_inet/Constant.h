1543235e2d3f44b568a2a6cb4da065c0973082c5
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2022-04-07 04:10:53 +0200

    declare a static function

--- src/veins_inet/Constant.h
+++ src/veins_inet/Constant.h
@@ -16,12 +16,30 @@
 #ifndef VEINS_INET_CONSTANT_H_
 #define VEINS_INET_CONSTANT_H_
 
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <stdio.h>
+
 class Constant {
 public:
     static constexpr const char* FIRST = "First vehicle";
     static constexpr const char* RSU_IDENTIFY = "RSU";
+
     Constant();
     virtual ~Constant();
 };
+
+static char* mergeContent(long Id){
+    char *cstr = new char[strlen(Constant::RSU_IDENTIFY) + 1];
+    strcpy(cstr, Constant::RSU_IDENTIFY);
+    std::string str = std::to_string(Id);
+    char *new_str = new char[str.length() + 1];
+    strcpy(new_str, str.c_str());
+    char *ret = new char[strlen(Constant::RSU_IDENTIFY) + strlen(new_str) + 1];
+    strcpy(ret, cstr);
+    strcat(ret, new_str);
+    return ret;
+}
 
 #endif /* VEINS_INET_CONSTANT_H_ */
