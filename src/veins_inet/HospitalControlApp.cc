6393597c6c80e5bf21007aa57e03ef5bd85d940f
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2022-04-20 16:16:40 +0700

    Controller for RSU of hospital

---
+++ src/veins_inet/HospitalControlApp.cc
@@ -0,0 +1,96 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "HospitalControlApp.h"
+
+#include "Constant.h"
+
+#include "veins/modules/application/traci/TraCIDemo11pMessage_m.h"
+
+using namespace veins;
+
+Register_Class(HospitalControlApp);
+
+void HospitalControlApp::initialize(int stage)
+{
+    TraCIDemoRSU11p::initialize(stage);
+    if (stage == 0) {
+        sendBeacon= new cMessage("send Beacon");
+    }
+    else if (stage == 1) {
+        // Initializing members that require initialized other modules goes here
+    }
+}
+
+void HospitalControlApp::finish()
+{
+    //Duoc goi khi RSU ket thuc cong viec
+    TraCIDemoRSU11p::finish();
+    // statistics recording goes here
+}
+
+void HospitalControlApp::onBSM(DemoSafetyMessage* bsm)
+{
+    //for my own simulation circle
+}
+
+void HospitalControlApp::onWSM(BaseFrame1609_4* wsm)
+{
+    // Your application has received a data message from another car or RSU
+    // code for handling the message goes here, see TraciDemo11p.cc for examples
+    cPacket* enc = wsm->getEncapsulatedPacket();
+    if(TraCIDemo11pMessage* bc = dynamic_cast<TraCIDemo11pMessage*>(enc)){
+        if(strcmp(Constant::FIRST, bc->getDemoData()) == 0
+                && wsm->getKind() == 0
+        ){
+            //EV << "my message = " <<bc->getDemoData()<<" from "<<bc->getSenderAddress()<<endl;
+            if(sendBeacon != NULL){
+                if (sendBeacon->isScheduled())
+                {
+                    cancelEvent(sendBeacon);
+                }
+
+                TraCIDemo11pMessage* rsuBeacon = new TraCIDemo11pMessage();
+
+                char *ret = mergeContent(bc->getSenderAddress());
+                rsuBeacon->setDemoData(ret);
+                rsuBeacon->setSenderAddress(myId);
+                BaseFrame1609_4* WSM = new BaseFrame1609_4();
+                WSM->encapsulate(rsuBeacon);
+                populateWSM(WSM);
+                send(WSM,lowerLayerOut);
+            }
+        }
+    }
+}
+
+void HospitalControlApp::onWSA(DemoServiceAdvertisment* wsa)
+{
+    // Your application has received a service advertisement from another car or RSU
+    // code for handling the message goes here, see TraciDemo11p.cc for examples
+}
+
+void HospitalControlApp::handleSelfMsg(cMessage* msg)
+{
+    TraCIDemoRSU11p::handleSelfMsg(msg);
+}
+
+void HospitalControlApp::handlePositionUpdate(cObject* obj)
+{
+    TraCIDemoRSU11p::handlePositionUpdate(obj);
+    // the vehicle has moved. Code that reacts to new positions goes here.
+    // member variables such as currentPosition and currentSpeed are updated in the parent class
+
+}
