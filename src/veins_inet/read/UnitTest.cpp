c7b30baaa6ea0d52aa74c989a2a3993a9a00c8c7
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2022-04-27 14:59:17 +0700

    unit test

---
+++ src/veins_inet/read/UnitTest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+#include <assert.h>
+#include "HashAPI.cpp"
+
+void assertAllLanes(int N, Lane *allLanes){
+    for(int i = 0; i < N; i++){
+        assert(allLanes[i].length > 0);
+        size_t size = sizeof(allLanes[i].id) / sizeof(allLanes[i].id[0]);
+        assert(size == 4);
+        if(i % 2 == 1)
+            assert(allLanes[i].id[0] == '-');
+        else{
+            assert(allLanes[i].id[0] != '-');
+        }
+    }
+}
