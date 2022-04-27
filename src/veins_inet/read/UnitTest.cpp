4ed8bb439255fed8a48bd46a96bf863ec4bc326c
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2022-04-27 15:00:34 +0700

    adjust an assert must be >= instead of ==

--- src/veins_inet/read/UnitTest.cpp
+++ src/veins_inet/read/UnitTest.cpp
@@ -10,7 +10,7 @@ void assertAllLanes(int N, Lane *allLanes){
     for(int i = 0; i < N; i++){
         assert(allLanes[i].length > 0);
         size_t size = sizeof(allLanes[i].id) / sizeof(allLanes[i].id[0]);
-        assert(size == 4);
+        assert(size >= 4);
         if(i % 2 == 1)
             assert(allLanes[i].id[0] == '-');
         else{
