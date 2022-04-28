0548c846a4171741e6f01aab5152dc8b3cc40ebc
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2022-04-28 13:46:53 +0700

    add a new static function: split

--- src/veins_inet/Constant.h
+++ src/veins_inet/Constant.h
@@ -47,4 +47,20 @@ static char* mergeContent(long Id){
     return ret;
 }
 
+static std::vector<std::string> split(const std::string& str, const std::string& delim)
+{
+    std::vector<std::string> tokens;
+    size_t prev = 0, pos = 0;
+    do
+    {
+        pos = str.find(delim, prev);
+        if (pos == std::string::npos) pos = str.length();
+        std::string token = str.substr(prev, pos-prev);
+        if (!token.empty()) tokens.push_back(token);
+        prev = pos + delim.length();
+    }
+    while (pos < str.length() && prev < str.length());
+    return tokens;
+}
+
 #endif /* VEINS_INET_CONSTANT_H_ */
